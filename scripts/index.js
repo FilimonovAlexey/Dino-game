a9ef559a2608470a00972dce7784e2d5f0e59246
Алексей Филимонов <alexeyf08@gmail.com>
2022-08-23 13:57:51 +0700

    feat: релиз

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
-document.addEventListener("keydown", function(event) {
-    jump();
-});
-
-function jump () {
+function jump() {
     if (dino.classList != "jump") {
-        dino.classList.add("jump")
+      dino.classList.add("jump");
+  
+      setTimeout(function () {
+        dino.classList.remove("jump");
+      }, 300);
     }
-    setTimeout(function() {
-       dino.classList.remove("jump")
-    }, 300)
-}
+  }
 
-let isAlive = setInterval (function() {
-    let dinoTop = parsInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    let cactusLeft = parsInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+let isAlive = setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
         alert("GAME OVER!")
     }
 }, 10)
+
+document.addEventListener("keydown", function (event) {
+    jump();
+  });
